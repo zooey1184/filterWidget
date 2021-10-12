587ce51ccc7505cc0746add11f15b5050729705e
yingyingzhang <zooey.zhang@igg.com>
2021-10-12 11:20:30 +0800

    feat: delete build

--- src/components/ContentModal/index.svelte
+++ src/components/ContentModal/index.svelte
@@ -25,7 +25,7 @@
   </div>
 
   <div class="btnWrap mt-16">
-    <div class="float-r">
+    <div class="flex flex-align-center flex-justify-end">
       <div class="btn round-2 btn-default" on:click={handleCancel}>
         {cancelTxt}
       </div>
